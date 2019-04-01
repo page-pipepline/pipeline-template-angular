a069792ce69786657e874c4bcb19bc1d0c945ba4
cntchen <cntchen@gmail.com>
2019-04-01 21:32:42 +0800

    feat: add ci server mode

---
+++ config/webpack.server.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const config = require('./index');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpackConfig = {
+  mode: 'production',
+  entry: {
+    styles: './src/styles.less',
+    polyfills: './src/polyfills.ts',
+    main: './src/main.ts'
+  },
+  // 忽略组件列表文件的打包
+  externals: {
+    './config/components': {
+      commonjs2: '../config/components.json',
+      commonjs: '../config/components.json',
+    },
+  },
+  output: {
+    path: path.join(__dirname, '..', 'server', 'dist'),
+    publicPath: './',
+    filename: '[name].[hash:8].js',
+    chunkFilename: '[id].chunk.[hash:8].js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          'babel-loader',
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              configFileName: path.join(__dirname, '../tsconfig.json')
+            }
+          },
+          'angular2-template-loader',
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        // 需要排除 src/index.html 文件, 交由 hmtl-webpack-plugin 处理
+        exclude: [/src\/index\.html$/]
+      },
+      {
+        // 全局样式需要用 style-loader
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+        // 只包含需要使用 style-loader 的 less 文件
+        include: path.resolve(__dirname, '../src/style.less')
+      },
+      {
+        // 组件中使用的 style 要用 to-string-loader 来处理
+        test: /\.(less)$/,
+        use: [
+          'to-string-loader',
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+        // 排除需要使用 style-loader 的 less 文件
+        exclude: path.resolve(__dirname, '../src/style.less')
+      },
+    ],
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: path.join(__dirname, '..', 'server', 'dist', 'index-origin.html'),
+        inject: true,
+        minify: false,
+        ...config.server.HWPPageBaseConfigForServer,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+}
+
+module.exports = webpackConfig;
---
+++ scripts/build-server.js
@@ -0,0 +1,32 @@
+require('./server-check-schema.js');
+require('./server-copy-config.js');
+require('./server-generate-library-info.js');
+
+process.env.NODE_ENV = 'production';
+
+const chalk = require('chalk');
+const webpack = require('webpack');
+const webpackConfig = require('../config/webpack.server.config');
+
+
+webpack(webpackConfig, (err, stats) => {
+  if (err) throw err;
+  process.stdout.write(`${stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  })}\n\n`);
+
+  if (stats.hasErrors()) {
+    console.log(chalk.red('  Build failed with errors.\n'));
+    process.exit(1);
+  }
+
+  console.log(chalk.cyan('  Build complete.\n'));
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ));
+});
